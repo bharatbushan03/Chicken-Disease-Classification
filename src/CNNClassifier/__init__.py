e0230ddc2ea85ff1d449743cd4a3fc4b752d6fc2
bharat03 <bharatbushan5320@gmail.com>
2026-05-27 23:28:21 +0530

    docs: add module-level docstring to CNNClassifier package

--- src/CNNClassifier/__init__.py
+++ src/CNNClassifier/__init__.py
@@ -1,3 +1,7 @@
+"""
+CNNClassifier package for Chicken Disease Classification.
+Provides modules for data ingestion, model preparation, training, and evaluation.
+"""
 import logging
 
 logging.basicConfig(
